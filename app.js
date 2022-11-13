ea26eee3c5127c2a7ae52f0994ea6e4e370cde65
center-coin <centercoinkr@gmail.com>
2022-11-13 21:22:51 +0500

    Adding files in repository

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Testing CI-CD pipe line with AWS code deploy and Nodejs(express) Thank you!! </h1> <h4>Message: Success</h4> <p>Version 1.4</p>');
+})
+
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
+})
+
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
+})
+ 
