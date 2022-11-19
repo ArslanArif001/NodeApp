e00854738323d4e6d191f7183cfcd4af9ab39802
center-coin <centercoinkr@gmail.com>
2022-11-19 22:19:32 +0500

    Deploying on Ubuntu 20

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing node servers"
-pkill node
+cd /var/www/
+sudo apt-get update
+curl -s https://deb.nodesource.com/setup_16.x | sudo bash
+sudo apt-get install nodejs -y
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16.17.0
+cd /var/www/
 
-#create our working directory if it doesnt exist
-DIR="/home/ubuntu/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+sudo npm install -f
